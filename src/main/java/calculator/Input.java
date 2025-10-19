7bcbca1f1459233b27a21e029f1d46da48d7ede1
believeme <cain1227@naver.com>
2025-10-19 23:28:06 +0900

    feat(io): 사용자 입력 및 결과 출력 기능 구현

---
+++ src/main/java/calculator/Input.java
@@ -0,0 +1,11 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Input {
+
+	public static String readInput() {
+		System.out.println("덧셈할 문자열을 입력하세요.");
+		return Console.readLine();
+	}
+}
---
+++ src/main/java/calculator/Output.java
@@ -0,0 +1,8 @@
+package calculator;
+
+public class Output {
+
+	public static void printResult(String result) {
+		System.out.println("결과 : " + result);
+	}
+}
